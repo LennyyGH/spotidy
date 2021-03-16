b77a8e9008e2af916c081cfb64c9fd6c38c0ab31
Lennard <lennard_445@hotmail.com>
2021-03-16 21:52:35 +0100

    added spotidy v 0.1.0 to public repo LennyyGH/spotidy

---
+++ vignettes/Connecting-with-the-Spotify-API.R
@@ -0,0 +1,12 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, eval = F----------------------------------------------------------
+#  library(spotidy)
+
+## ----get-token, eval = F------------------------------------------------------
+#  my_token <- get_spotify_api_token(client_id = "***", client_secret = "***")
+
